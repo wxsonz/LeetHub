63d1c1fded1ab7ef3d9419150249acb22e07adb6
wxz <109400750+wxsonz@users.noreply.github.com>
2023-12-06 02:55:19 +0700

    Time: 14 ms (47.46%), Space: 7.3 MB (35.85%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.c
@@ -0,0 +1,27 @@
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+
+    int length = nums1Size + nums2Size;
+    int hl = length / 2;
+    int merged[length];
+    int i = 0, j = 0, k = 0;
+
+    for (; i < nums1Size && j < nums2Size;)
+    {
+        if (nums1[i] < nums2[j])
+            merged[k++] = nums1[i++];
+        else
+            merged[k++] = nums2[j++];
+    }
+
+    for (; i < nums1Size;)
+        merged[k++] = nums1[i++];
+    
+    for (; j < nums2Size;)
+        merged[k++] = nums2[j++];
+    
+    if (length % 2 ==0)
+        return (double)(merged[hl - 1] + merged[hl]) / 2.0;
+    else
+        return (double)merged[length / 2];
+    
+}
