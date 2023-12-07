a601b2db4744093c77c59ec7fb127f1d5064a17b
wxz <109400750+wxsonz@users.noreply.github.com>
2023-12-08 04:17:58 +0700

    Time: 4 ms (94.51%), Space: 10.2 MB (14.29%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.c
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.c
@@ -4,15 +4,20 @@ char* largestOddNumber(char* num) {
     // find index of odd number from backward
     // copy fron num to new num until that found index
     
-    int odd_index, length = strlen(num);
+    int length = strlen(num);
     
+    // further optimization
     // for the case that last odd index is at the last character
     if ((num[length - 1] - '0') % 2 == 1)
         return num;
     
-    char* new_num = malloc(length + 1);
+    // if not then skip the last character
+    int odd_index = length - 2;
     
-    for (odd_index = length - 1; odd_index >= 0; odd_index--)
+    // +2 to compensate the length we -2 and reserve space for '\0'
+    char* new_num = malloc(odd_index + 2);
+    
+    for (; odd_index >= 0; odd_index--)
         if (num[odd_index] % 2 == 1) 
             break;
         
