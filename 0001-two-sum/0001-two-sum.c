50e7614d1230eda6dda7601d9b1463576097f46c
wxz <109400750+wxsonz@users.noreply.github.com>
2023-12-12 15:29:53 +0700

    Time: 51 ms (87.51%), Space: 7.3 MB (10.10%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,22 +1,41 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
+typedef struct {
+    int val;
+    int index;
+} HashTable;
+
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     
-    //monke #2
+    // check what current_number it need to reach the target and store it
+    // go next
+    // check what current_number it need to reach the target and store it
+    // check if we found any stored_number
+    // and when it is found we return that
     
+    HashTable* hashTable = (HashTable*)malloc(numsSize * sizeof(HashTable));
     int* result = (int*)malloc(2 * sizeof(int));
-    
-    for (int i = 0; i < numsSize - 1; i++) {
-        for (int j = i + 1; j < numsSize; j++) {
-            if (nums[i] + nums[j] == target) {
-                result[0] = i;
-                result[1] = j;
+
+    for (int i = 0; i < numsSize; i++) {
+        int complement = target - nums[i];
+
+        for (int j = 0; j < i; j++) {
+            if (hashTable[j].val == complement) {
+                result[0] = hashTable[j].index;
+                result[1] = i;
                 *returnSize = 2;
+                free(hashTable);
                 return result;
             }
         }
+
+        // If the complement does not exist, add the current number to the hash table
+        hashTable[i].val = nums[i];
+        hashTable[i].index = i;
     }
-    
+
+    free(hashTable);
     return result;
 }
