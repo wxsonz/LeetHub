effa91617d60a7677c2a0b96d55e0cad5d63acf7
wxz <109400750+wxsonz@users.noreply.github.com>
2023-12-13 21:00:32 +0700

    Time: 20 ms (60.71%), Space: 7.9 MB (7.14%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.c
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.c
@@ -5,11 +5,17 @@ int numSpecial(int** mat, int matSize, int* matColSize) {
     int rows = matSize;
     int cols = *matColSize;
     
-    int countX[cols];
-    int countY[rows];
+    //int countX[cols];
+    //int countY[rows];
     
-    memset(countX, 0, sizeof(countX));
-    memset(countY, 0, sizeof(countY));
+    int* countX = (int*)malloc(sizeof(int) * cols);
+    int* countY = (int*)malloc(sizeof(int) * rows);
+    
+    //memset(countX, 0, sizeof(countX));
+    //memset(countY, 0, sizeof(countY));
+    
+    memset(countX, 0, cols * sizeof(int));
+    memset(countY, 0, rows * sizeof(int));
 
     for (int y = 0; y < rows; y++) {
         for (int x = 0; x < cols; x++) {
