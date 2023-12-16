c3dc892c00a733fcd0519e9d8487d9f6538bd285
wxz <109400750+wxsonz@users.noreply.github.com>
2023-12-16 14:19:03 +0700

    Time: 4 ms (66.53%), Space: 6.7 MB (56.40%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.c
@@ -0,0 +1,28 @@
+#define MAX_CHARS 26
+
+bool isAnagram(char* s, char* t) {
+    
+    int s_len = strlen(s);
+    int t_len = strlen(t);
+    
+    if (s_len != t_len) return 0;
+    
+    int s_count[MAX_CHARS] = {0};
+    int t_count[MAX_CHARS] = {0};
+
+    for (int i = 0; i < s_len; i++) {
+        s_count[s[i] - 'a']++;
+    }
+
+    for (int i = 0; i < t_len; i++) {
+        t_count[t[i] - 'a']++;
+    }
+
+    for (int i = 0; i < MAX_CHARS; i++) {
+        if (s_count[i] != t_count[i]) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
