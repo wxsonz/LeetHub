003a7e909a8e8cd32255183d1e9eb0705c157dee
wxz <109400750+wxsonz@users.noreply.github.com>
2023-12-20 18:26:39 +0700

    Time: 8 ms (47.50%), Space: 6.8 MB (10.00%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.c
@@ -0,0 +1,28 @@
+int buyChoco(int* prices, int pricesSize, int money){
+    int min1, min2;
+    // min1 < min2
+    
+    if (prices[0] < prices[1]) {
+        min1 = prices[0];
+        min2 = prices[1];
+    }
+    else {
+        min1 = prices[1];
+        min2 = prices[0];
+    }
+    
+    for (int i = 2; i < pricesSize; i++) {
+        if (prices[i] < min1) {
+            min2 = min1;
+            min1 = prices[i];
+        } else if (prices[i] < min2) {
+            min2 = prices[i];
+        }
+    }
+    
+    if (min1 + min2 <= money) {
+        return money - (min1 + min2);
+    }
+    
+    return money;
+}
