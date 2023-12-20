2c329bce0ec2519439bf527ed3364c02ea872b69
wxz <109400750+wxsonz@users.noreply.github.com>
2023-12-20 18:39:28 +0700

    Time: 4 ms (77.50%), Space: 6.6 MB (22.50%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.c
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.c
@@ -1,34 +1,21 @@
 int buyChoco(int* prices, int pricesSize, int money){
     
-    int *min1 = (int*)malloc(sizeof(int));
-    int *min2 = (int*)malloc(sizeof(int));
-    int *minT = (int*)malloc(sizeof(int));
-    
-    *min1 = 100;
-    *min2 = 100;
+    int min1 = 100, min2 = 100;
     
     for (int i = 0; i < pricesSize; i++) {
-        if (prices[i] < *min1) {
-            *min2 = *min1;
-            *min1 = prices[i];
-        } else if (prices[i] < *min2) {
-            *min2 = prices[i];
+        if (prices[i] < min1) {
+            min2 = min1;
+            min1 = prices[i];
+        } else if (prices[i] < min2) {
+            min2 = prices[i];
         }
     }
     
-    *minT = *min1 + *min2;
+    int minT = min1 + min2;
     
-    if (*minT <= money) {
-        free(min1);
-        free(min2);
-        int result = money - *minT;
-        free(minT);
-        return result;
+    if (minT <= money) {
+        return money - minT;
     }
-    
-    free(min1);
-    free(min2);
-    free(minT);
-    
+
     return money;
 }
