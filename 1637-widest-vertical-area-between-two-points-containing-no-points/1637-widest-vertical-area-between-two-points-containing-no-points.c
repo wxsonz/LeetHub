cf70e75436c888bfa7e6f2cad3460d78af25465f
wxz <109400750+wxsonz@users.noreply.github.com>
2023-12-21 20:06:37 +0700

    Time: 231 ms (15.38%), Space: 30 MB (23.08%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.c
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.c
@@ -4,13 +4,22 @@ int compare(const void* a, const void* b) {
 
 int maxWidthOfVerticalArea(int** points, int pointsSize, int* pointsColSize) {
     
+    int* xCoordinates = (int*)malloc(sizeof(int) * pointsSize);
+    
+    for (int i = 0; i < pointsSize; ++i) {
+        xCoordinates[i] = points[i][0];
+    }
+    
     qsort(points, pointsSize, sizeof(int*), compare);
 
     int res = 0;
+    
     for (int i = 1; i < pointsSize; ++i) {
         int diff = points[i][0] - points[i - 1][0];
         res = (diff > res) ? diff : res;
     }
+    
+    free(xCoordinates);
 
     return res;
 }
