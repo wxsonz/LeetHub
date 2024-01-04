6d47734aeabd1462e79af7093250cff8513c294f
wxz <109400750+wxsonz@users.noreply.github.com>
2024-01-04 11:52:38 +0700

    Time: 184 ms (90.91%), Space: 16.3 MB (27.27%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.c
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.c
@@ -1,16 +1,20 @@
-int compare(const void *a, const void *b) {
+int compare(const void *a, const void *b) 
+{
     return (*(int *)a - *(int *)b);
 }
 
-int minOperations(int* nums, int numsSize) {
+int minOperations(int* nums, int numsSize) 
+{
     qsort(nums, numsSize, sizeof(int), compare);
 
     int res = 0;
     int si = 0;
-    while (si < numsSize) {
+    while (si < numsSize) 
+    {
         int ei = si;
 
-        while (ei < numsSize && nums[ei] == nums[si]) {
+        while (ei < numsSize && nums[ei] == nums[si]) 
+        {
             ei++;
         }
         int count = ei - si;
